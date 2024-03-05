0ad5847cbee89c49f7d76a485056c7c38e76190d
VartikaJain-17 <133216343+VartikaJain-17@users.noreply.github.com>
2024-03-05 16:55:55 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+
+//Paste your firebaseConfig here
+const firebaseConfig = {
+    apiKey: "AIzaSyCWQYk3iC1yxx8lAYs5asXcpxTjvHG-klo",
+    authDomain: "e-ride-app-a131a.firebaseapp.com",
+    projectId: "e-ride-app-a131a",
+    storageBucket: "e-ride-app-a131a.appspot.com",
+    messagingSenderId: "157463905577",
+    appId: "1:157463905577:web:83f94fa03aadd75f672338"
+  };
+//
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
